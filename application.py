c142d7cabd7fabd3fa5e871fea7b55accad9998f
mk@Ehrmantraut <37540514+mkfallen@users.noreply.github.com>
2018-09-09 16:41:39 +0300

    Implemented "channel creation" and "channel list"

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -8,12 +8,24 @@ app.config["JSON_SORT_KEYS"] = False
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+channels = []
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", channels=channels)
 
 
-@socketio.on("set name")
+@socketio.on("create channel")
 def name(data):
-    return("Hehe")
+    channel = {}
+    channel["id"] = len(channels) + 1
+    channel["name"] = data["name"]
+    channel["creator"] = data["creator"]
+    channels.append(channel)
+    emit("channel list", channels, broadcast=True)
+
+
+@socketio.on("get channel list")
+def get_channel_list(data):
+    emit("channel list", channels, broadcast=True)
--- static/index.js
+++ static/index.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  if(!localStorage.getItem('username')){
+  if (localStorage.getItem('username')) {
+    document.querySelector('#name-display').innerHTML = 'Flack: '+localStorage.getItem('username')
+  }
+  else {
     document.getElementById('name-form').style.display = 'block';
     document.getElementById('name-input').onkeyup = function(){
       if(this.value.length > 0){
@@ -9,18 +12,61 @@ document.addEventListener('DOMContentLoaded', () => {
       else{
         document.getElementById('name-button').disabled = true;
       }
-    };
+    }
     document.querySelector('#name-form > form').onsubmit = () => {
       const name = document.getElementById('name-input').value;
       $('.alert').alert('close');
       localStorage.setItem('username', name);
-      document.getElementById('name-display').innerHTML = localStorage.getItem('username');
+      document.getElementById('name-display').innerHTML = 'Flack: '+localStorage.getItem('username');
       return false;
     };
   };
 
-  document.querySelector('#name-display').innerHTML = localStorage.getItem('username');
-
   // Connect to websocket
   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+  socket.on('connect', () => {
+    document.querySelectorAll('.channel').forEach(button => {
+      button.onclick = () => {
+        const action = button.dataset.action;
+        if (action === 'create') {
+          if (!document.getElementById('channel-name')){
+            const newchannel = `
+              <div class="p-3 border-bottom">
+                <input id="channel-name" class="form-control form-control-sm" type="text" autofocus="true" placeholder="Channel name">
+              </div>
+            `
+            document.getElementById('channel-list').innerHTML += newchannel;
+            document.getElementById('new-channel').innerHTML = '<strong>✓</strong> Create';
+          }
+          else if (document.getElementById('channel-name').value){
+            socket.emit('create channel', {
+              'creator': localStorage.getItem('username'),
+              'name': document.getElementById('channel-name').value
+            });
+            document.getElementById('new-channel').innerHTML = '<strong>+</strong> New channel';
+          }
+        }
+        else if (action === 'channel' && button.dataset.id) {
+          socket.emit('get messages', {
+            'id': button.dataset.id
+          });
+        }
+      };
+    });
+    socket.emit('get channel list', {});
+  });
+
+
+  socket.on('channel list', data => {
+    var list = '';
+    for(var i=0; i < data.length; i++){
+      list += `
+      <div class="p-3 border-bottom channel" data-action="channel" data-id="${data[i]['id']}">
+        ${data[i]["name"]}
+      </div>
+      `
+    };
+    document.getElementById('channel-list').innerHTML=list;
+  });
 });
